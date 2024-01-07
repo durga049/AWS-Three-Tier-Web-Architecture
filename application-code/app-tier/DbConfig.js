607f5f6031c2656f4a864ad64feb35176dd9ac41
durgaprasad <durgaprasadgudipalli@gmail.com>
2024-01-07 21:57:18 +0530

    initial commit

---
+++ application-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'database-1-instance-1.c5fvnfnpfkuk.us-east-2.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : '12345678',
+    DB_DATABASE : 'webappdb'
+});
